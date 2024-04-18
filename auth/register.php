48b14f2bb0b8560e861dd0fe55ed6739fd9ea877
Lê Trọng Nhân <44769901+NathanNhan@users.noreply.github.com>
2024-04-18 11:32:45 +0700

    bai2:register

---
+++ auth/register.php
@@ -0,0 +1,71 @@
+
+<?php
+  require_once '../includes/header.php';
+  //Nhúng PDO vào để thao tác truy vấn xuống database
+  require_once '../config/config.php';
+  if(isset($_POST['submit'])) {
+    // $_GET['name'] -> lấy các giá trị của các input form thông qua phương thức GET 
+    // $_POST['name'] -> lấy ra các giá trị input nhập vào của form thông qua phương thức POST 
+    if($_POST['email'] == '' or $_POST['username'] == '' or $_POST['password'] == '') {
+      echo 'Please fill in one field or more';
+    } else {
+      $email = $_POST['email'];
+      $username = $_POST['username'];
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+      // Insert dữ liệu vào trong database
+      $insert = $conn->prepare("INSERT INTO users(email, username, password) VALUES (:email, :username, :password)");
+      $insert->execute([
+        ':email' => $email, 
+        ':username' => $username, 
+        ':password' => $password
+      ]);
+      
+      header('Location: login.html');
+
+    }
+  }
+?>
+                <!-- Main Content-->
+        <div class="container px-4 px-lg-5">
+
+            <form method="POST" action="register.php">
+              <!-- Email input -->
+              <div class="form-outline mb-4">
+                <input type="email" name="email" id="form2Example1" class="form-control" placeholder="Email" />
+               
+              </div>
+
+              <div class="form-outline mb-4">
+                <input type="" name="username" id="form2Example1" class="form-control" placeholder="Username" />
+               
+              </div>
+
+              <!-- Password input -->
+              <div class="form-outline mb-4">
+                <input type="password" name="password" id="form2Example2" placeholder="Password" class="form-control" />
+                
+              </div>
+
+
+
+              <!-- Submit button -->
+              <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">Register</button>
+
+              <!-- Register buttons -->
+              <div class="text-center">
+                <p>Aleardy a member? <a href="login.php">Login</a></p>
+                
+
+               
+              </div>
+            </form>
+
+
+           
+        </div>
+    <!-- Footer-->
+
+<?php
+  require_once '../includes/footer.php'
+?>
