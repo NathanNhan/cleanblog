13b8d4fc03e9873e66b34483ecc16bf568f0d42a
Lê Trọng Nhân <44769901+NathanNhan@users.noreply.github.com>
2024-04-22 13:23:19 +0700

    bai15:show_data_dashboard

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -7,6 +7,31 @@
     header('Location: ./admins/login-admins.php');
    }
 
+   //Tổng số bài viết 
+   $select = $conn->query("SELECT COUNT(*) as total_post from posts;");
+
+   $select->execute(); 
+
+   $totalPosts = $select->fetch(PDO::FETCH_OBJ);
+
+  //  print_r($totalPosts);
+
+
+   //Tổng số chuyên mục bài viết 
+   $select = $conn->query("SELECT COUNT(*) as total_category from category;");
+
+   $select->execute(); 
+
+   $totalCategories = $select->fetch(PDO::FETCH_OBJ);
+
+
+   //Tổng số tài khoản admin 
+   $select = $conn->query("SELECT COUNT(*) as total_admin from admin;");
+
+   $select->execute(); 
+
+   $totalAdmin = $select->fetch(PDO::FETCH_OBJ);
+
 
 ?>
 
@@ -18,7 +43,7 @@
             <div class="card-body">
               <h5 class="card-title">Posts</h5>
               <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
-              <p class="card-text">number of posts: 20</p>
+              <p class="card-text">number of posts: <?php echo $totalPosts->total_post; ?></p>
              
             </div>
           </div>
@@ -28,7 +53,7 @@
             <div class="card-body">
               <h5 class="card-title">Categories</h5>
               
-              <p class="card-text">number of categories: 25</p>
+              <p class="card-text">number of categories: <?php echo $totalCategories->total_category; ?></p>
               
             </div>
           </div>
@@ -38,7 +63,7 @@
             <div class="card-body">
               <h5 class="card-title">Admins</h5>
               
-              <p class="card-text">number of admins: 50</p>
+              <p class="card-text">number of admins: <?php echo $totalAdmin->total_admin; ?></p>
               
             </div>
           </div>
