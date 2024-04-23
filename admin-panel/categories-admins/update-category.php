933d2022cd2057f6e52f09c22af58162a92da081
Lê Trọng Nhân <44769901+NathanNhan@users.noreply.github.com>
2024-04-23 12:42:37 +0700

    bai23:update_category

--- admin-panel/categories-admins/update-category.php
+++ admin-panel/categories-admins/update-category.php
@@ -3,8 +3,37 @@
 
 
 <?php 
+     //Chặn (interceptors) không cho user cập nhật trực tiếp thông qua đường dẫn khi chưa đăng nhập
+    if(!isset($_SESSION['adminname'])) {
+      header ('location: http://cleanblog.test/admin-panel/admins/login-admins.php');
+    }
 
-    
+     if(isset($_GET['id'])) {
+            $id = $_GET['id'];
+            // Viết câu truy vấn query thông qua PDO 
+            $category = $conn->query("SELECT * FROM category where id = '$id'");
+            //Chạy câu truy vấn 
+            $category->execute();
+            //Convert sang đối tượng thông phương thức fetch()
+            $row = $category->fetch(PDO::FETCH_OBJ);
+            if(isset($_POST['submit'])) {
+              if($_POST['name'] == '') {
+                echo "<div class='alert alert-danger text-center' role='alert'>
+                           Please fill one or more fields
+                      </div>";
+              } else {
+                $name = $_POST['name'];
+                //Update xuống database
+                $update = $conn->prepare("UPDATE category SET name = :name where id = '$id'");
+                $update->execute([
+                    ':name' => $name,
+                ]);
+                header('location: show-categories.php');
+              }
+            }
+        } else {
+            header('location: http://cleanblog.test/404.php');
+        }
 
 
 
@@ -15,10 +44,10 @@
           <div class="card">
             <div class="card-body">
               <h5 class="card-title mb-5 d-inline">Update Categories</h5>
-          <form method="POST" action="" enctype="multipart/form-data">
+          <form method="POST" action="">
                 <!-- Email input -->
                 <div class="form-outline mb-4 mt-4">
-                  <input type="text"  name="name" id="form2Example1" class="form-control" placeholder="name" />
+                  <input type="text"  name="name" id="form2Example1" class="form-control" placeholder="name" value="<?php echo $row->name; ?>" />
                  
                 </div>
 
