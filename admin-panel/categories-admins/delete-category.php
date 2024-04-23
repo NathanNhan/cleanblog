20956dd56499c682b1e5fbf2669cecd9e26a49f7
Lê Trọng Nhân <44769901+NathanNhan@users.noreply.github.com>
2024-04-23 12:23:43 +0700

    bai22:delete_category

--- admin-panel/categories-admins/delete-category.php
+++ admin-panel/categories-admins/delete-category.php
@@ -1,10 +1,24 @@
-<?php //require "../includes/navbar.php"; ?>
+
 
 
 <?php require "../../config/config.php"; ?>
 
 <?php 
-
+  //Chặn không cho user xóa trực tiếp thông qua đường dẫn khi chưa đăng nhập
+  if(!isset($_SESSION['adminname'])) {
+    header ('location: http://cleanblog.test/admin-panel/admins/login-admins.php');
+   }
+  
+  if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $category_delete = $conn->prepare("DELETE FROM category where id = :id");
+    $category_delete->execute([
+        ':id' => $id
+    ]);
+    header('location: show-categories.php');
+  } else {
+    header('location: http://cleanblog.test/404.php');
+  }
  
 
 ?>
