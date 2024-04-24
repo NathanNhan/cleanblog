84df298c69577ff6facda68cef111c9e769e78ca
Lê Trọng Nhân <44769901+NathanNhan@users.noreply.github.com>
2024-04-24 21:16:49 +0700

    bai25:pagination_P1

--- admin-panel/posts-admins/show-posts.php
+++ admin-panel/posts-admins/show-posts.php
@@ -1,10 +1,22 @@
 <?php require "../layouts/header.php"; ?>
 <?php require "../../config/config.php"; ?>
 <?php 
+    //Mặc định lần đầu tiên hiển thị thì page = 1 
+    $start_from = 0;
+    //Giới hạn số bài viết hiển thị cho 1 trang 
+    $posts_per_page = 2;
+    $page = 1; 
+    if(isset($_GET['page'])) {
+      $page = intval($_GET['page']);
+      //Tính vị trí bắt đầu lấy dữ liệu
+      $start_from = ( $page - 1 ) * $posts_per_page;
+    } 
     
-    $select = $conn->query("SELECT posts.id, posts.title, category.name, posts.user_name from posts inner join category on posts.category_id = category.id");
-    $select->execute();
-    $row = $select->fetchAll(PDO::FETCH_ASSOC);
+
+        
+      $select = $conn->query("SELECT posts.id, posts.title, category.name, posts.user_name from posts inner join category on posts.category_id = category.id LIMIT $start_from , $posts_per_page");
+      $select->execute();
+      $row = $select->fetchAll(PDO::FETCH_ASSOC);
 
    
 
@@ -37,12 +49,36 @@
                       <th><a href="" class="btn btn-danger text-center">Delete</a></th>
                     </tr>
                    <?php endforeach; ?>
-                </tbody>
-              </table> 
+
+                   </tbody>
+                   </table> 
+                   <!-- Pagination -->
+                   
+                   <nav aria-label="...">
+                      <ul class="pagination pagination-lg">
+                        <?php
+                           $totalPost = $conn->query("SELECT count(*) as total from posts;");
+                           $totalPost->execute();
+                           $total_posts = $totalPost->fetch(PDO::FETCH_OBJ);
+                           
+                           
+
+                           $pagination_number = ceil((intval($total_posts->total) / $posts_per_page));
+                          
+                           //Vòng Lặp để in ra số phân trang
+                           for ($i=1; $i <= $pagination_number ; $i++) { 
+                            ?>
+                             <li class="page-item"><a class="page-link" href="show-posts.php?page=<?php echo $i ?>"><?php echo $i; ?></a></li>
+                            
+                            <?php 
+                           }
+                        
+                        ?>
+                      </ul>
+                  </nav>
             </div>
           </div>
         </div>
 </div>
-
 
 <?php require "../layouts/footer.php"; ?>
